119835784ef58896cbc4d16def81b1c46506f884
Tess Sluijter-Stek <tess@unixerius.nl>
2022-09-24 22:42:04 +0200

    Proper typing to hostname and protocol

--- solutions.py
+++ solutions.py
@@ -11,19 +11,19 @@ from products import solve_product_challenges
 from users import solve_user_challenges
 
 parser = argparse.ArgumentParser(description='Run JuiceShop solver against a target.')
-parser.add_argument('--hostname', nargs='?', type=ascii, 
+parser.add_argument('--hostname', nargs='?',
                     const='localhost', default='localhost',
                     help='The FQDN of the target host (default: localhost)')
 parser.add_argument('--port', nargs='?', type=int, 
                     const=3000, default=3000,
                     help='The TCP port for JuiceShop (default: 3000)')
-parser.add_argument('--protocol', nargs='?', type=ascii, 
+parser.add_argument('--protocol', nargs='?',
                     const='http', default='http',
                     help='Indicate [http/https] (default: http)')
 
 args = parser.parse_args()
 
-server = args.protocol+'://'+args.hostname+str(args.port)
+server = args.protocol+'://'+args.hostname+':'+str(args.port)
 #server = 'http://localhost:3000'
 #server = 'http://localhost:8080'
 
